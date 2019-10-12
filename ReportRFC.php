742b40a35522d7312c826f4650d990313b860d6c
ZwiityDev <49972937+tiwloveyou03@users.noreply.github.com>
2019-10-12 12:26:22 +0700

    ee
    sdszs

--- ReportRFC.php
+++ ReportRFC.php
@@ -103,13 +103,6 @@
                 <i class="fas fa-fw fa-table"></i>
                 <span>TABLE for customer</span></a>
         </li>
-
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Charts</span></a>
-        </li>
         <!-- Nav Item - report -->
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsereport" aria-expanded="true" aria-controls="collapsePages">
@@ -163,18 +156,6 @@
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Profile
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Settings
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Activity Log
-                            </a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -271,7 +252,7 @@
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
+                <a class="btn btn-primary" href="login.php">Logout</a>
             </div>
         </div>
     </div>
---
+++ checklogin.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+$ta = mysqli_connect("localhost","root", "", "miniprojectnawattakron");
+echo $_POST["username"];
+echo $_POST["password"];
+if(empty($_POST["username"]) and empty($_POST["password"])){
+    header("location:login.php");
+}else{
+    $strSQL="";
+    $strSQL = "SELECT * FROM admin WHERE username = '$_POST[username]' and password = '$_POST[password]'";
+    $objQuery = mysqli_query($ta,$strSQL);
+    $objResult = mysqli_fetch_array($objQuery);
+    // echo $objResult["id"];
+    if(!$objResult)
+    {
+        header("location:login.php");
+    }
+    else
+    {
+        $_SESSION["username"] = $objResult["username"];
+        $_SESSION["status"] = $objResult["status"];
+
+
+        // session_write_close();
+
+        if($objResult["status"] == "admin")
+        {
+            header("location:index.php");
+        }
+    }
+    mysqli_close($ta);
+}
+?>
